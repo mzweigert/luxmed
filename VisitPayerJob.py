47a8b722c7fa6b5fcf45c1c83fe1de0ee9ac9c8c
mzweigert <mateuszzweigert@gmail.com>
2020-02-29 12:58:40 +0100

    Add logs to visit payer job

--- VisitPayerJob.py
+++ VisitPayerJob.py
@@ -22,12 +22,17 @@ def init():
 def action():
     visits = DBManager.get_all_visits()
     if not visits:
+        print("No visits found")
         return
     for email, password in auth_data:
         if email not in visits:
             continue
         api = MedicalInsuranceApi(email, password, MedicalInsuranceType.LuxMed)
         for visit_to_book in visits[email]:
+            print("I am trying book a visit with service_id = {0} for user = {1}".format(visit_to_book.service_id, email))
             visit_to_book, details = api.book_a_visit(visit_to_book)
             if details:
+                print("Visit has been booked.")
                 DBManager.remove_visit(visit_to_book.db_record_id)
+            else:
+                print("Couldn't find visit at this moment.")
